aba76d2c8cfe950d9533b85fd0ed39fdff316eb3
Marina Ismailova <marina1988sherbakova@gmail.com>
2018-02-04 05:44:30 +0300

    master: fix build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     prefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
-    sass = require('gulp-less'),
+    less = require('gulp-less'),
     sourcemaps = require('gulp-sourcemaps'),
     rigger = require('gulp-rigger'),
     cssmin = require('gulp-minify-css'),
@@ -19,24 +19,24 @@ var gulp = require('gulp'),
 var path = {
     build: { //Тут мы укажем куда складывать готовые после сборки файлы
         html: 'build/',
-        // js: 'build/js/',
         css: 'build/css/',
-        img: 'build/img/',
+        img: 'build/img/'
         // php: 'build/form/'
+        // js: 'build/js/',
     },
     src: { //Пути откуда брать исходники
-        html: '*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
         // js: 'src/js/build.js',//В стилях и скриптах нам понадобятся только build файлы
-        style: 'styles/build.less',
-        img: 'img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        style: 'src/styles/build.less',
+        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
         // php: 'src/form/*.php'
     },
     watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
-        html: '**/*.html',
-        // js: 'src/js/**/*.js',
-        style: 'styles/**/*.less',
-        img: 'img/**/*.*',
+        html: 'src/**/*.html',
+        style: 'src/styles/**/*.less',
+        img: 'src/img/**/*.*'
         // php: 'src/form/*.php'
+        // js: 'src/js/**/*.js',
     },
     clean: './build'
 };
@@ -58,29 +58,29 @@ gulp.task('html:build', function () {
         .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
 });
 
-// gulp.task('js:build', function () {
-//     gulp.src(path.src.js) //Найдем наш main файл
-//         .pipe(rigger()) //Прогоним через rigger
-//         .pipe(sourcemaps.init()) //Инициализируем sourcemap
-//         .pipe(uglify()) //Сожмем наш js
-//         .pipe(sourcemaps.write()) //Пропишем карты
-//         .pipe(rename({suffix: '.min'}))
-//         .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
-//         .pipe(reload({stream: true})); //И перезагрузим сервер
-// });
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) //Найдем наш main файл
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(sourcemaps.init()) //Инициализируем sourcemap
+        .pipe(uglify()) //Сожмем наш js
+        .pipe(sourcemaps.write()) //Пропишем карты
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); //И перезагрузим сервер
+});
 
-// gulp.task('php:build', function () {
-//     gulp.src(path.src.php) //Найдем наш main файл
-//         .pipe(sourcemaps.init()) //Инициализируем sourcemap
-//         .pipe(sourcemaps.write()) //Пропишем карты
-//         .pipe(gulp.dest(path.build.php)) //Выплюнем готовый файл в build
-//         .pipe(reload({stream: true})); //И перезагрузим сервер
-// });
+gulp.task('php:build', function () {
+    gulp.src(path.src.php) //Найдем наш main файл
+        .pipe(sourcemaps.init()) //Инициализируем sourcemap
+        .pipe(sourcemaps.write()) //Пропишем карты
+        .pipe(gulp.dest(path.build.php)) //Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); //И перезагрузим сервер
+});
 
 gulp.task('style:build', function () {
     gulp.src(path.src.style) //Выберем наш main.scss
         .pipe(sourcemaps.init()) //То же самое что и с js
-        .pipe(sass()) //Скомпилируем
+        .pipe(less()) //Скомпилируем
         .pipe(prefixer()) //Добавим вендорные префиксы
         .pipe(cssmin()) //Сожмем
         .pipe(sourcemaps.write())
@@ -103,9 +103,9 @@ gulp.task('image:build', function () {
 
 gulp.task('build', [
     'html:build',
-    // 'js:build',
     'style:build',
-    'image:build',
+    'image:build'
+    // 'js:build',
     // 'php:build',
 ]);
 
@@ -116,17 +116,20 @@ gulp.task('watch', function(){
     watch([path.watch.style], function(event, cb) {
         gulp.start('style:build');
     });
-    // watch([path.watch.js], function(event, cb) {
-    //     gulp.start('js:build');
-    // });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
     watch([path.watch.img], function(event, cb) {
         gulp.start('image:build');
     });
+    watch([path.watch.php], function(event, cb) {
+        gulp.start('php:build');
+    });
 });
 
-// gulp.task('webserver', function () {
-//     browserSync(config);
-// });
+gulp.task('webserver', function () {
+    browserSync(config);
+});
 
 gulp.task('clean', function (cb) {
     rimraf(path.clean, cb);
